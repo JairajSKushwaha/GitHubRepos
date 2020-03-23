cbe0b89f7224f3e1e69b384eb7045f70e168d3d6
JairajSKushwaha <61612297+JairajSKushwaha@users.noreply.github.com>
2020-03-22 23:01:25 -0700

    Update Program.cs

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApplication
 {
     class Program
     {
+        // Execution start from here.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
