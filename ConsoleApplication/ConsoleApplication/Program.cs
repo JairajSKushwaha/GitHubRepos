c6e27f62343377b89bd19e8f0015db9beddce809
Jairaj Kushwaha <jairajkushwaha@hotmail.com>
2020-03-24 05:11:52 -0700

    c

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-// this is a namespace
 namespace ConsoleApplication
 {
     class Program
     {
-        // removed the comments
         static void Main(string[] args)
         {
+            // 1
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
